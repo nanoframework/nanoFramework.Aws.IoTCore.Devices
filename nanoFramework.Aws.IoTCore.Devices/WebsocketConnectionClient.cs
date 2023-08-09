5c41b9669ed5a5d3612f32fa810db196c6a87808
Robin Jones <networkfusion@users.noreply.github.com>
2023-08-09 20:48:51 +0100

    Add sigv4 initial commit
    Before we get real dirty.

--- nanoFramework.Aws.IoTCore.Devices/WebsocketConnectionClient.cs
+++ nanoFramework.Aws.IoTCore.Devices/WebsocketConnectionClient.cs
@@ -4,22 +4,56 @@
 //
 
 using System;
+using System.Net.WebSockets;
+using nanoFramework.Aws.SignatureVersion4;
 
-namespace nanoFramework.Aws.IoTCore.Devices.Devices.Client
+namespace nanoFramework.Aws.IoTCore.Devices
 {
     /// <summary>
-    /// AWS IoT Core Websocket Connection Client for .NET nanoFramework
+    /// AWS IoT Core MQTT over Websocket Connection Client for .NET nanoFramework
     /// </summary>
-    public class WebsocketConnectionClient// : IDisposable
+    public class WebsocketConnectionClient : IDisposable
     {
 
+        public Uri Host { get; set; }
+        public string Region { get; set; }
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        const int _wssPort = 443; //Default WSS port.
+
+        WebSocket _webSocket = null;
+
         /// <summary>
-        /// Creates a new WebSocket Connection Client
+        /// Creates a new MQTT over WebSocket Connection Client
         /// </summary>
+        /// <remarks>
+        /// Supports Signature Version 4 and Custom authentication over port 443
+        /// </remarks>
         public WebsocketConnectionClient()
         {
-            //TODO: implement!
+            // TODO: implement! look to the following:
+            // https://github.com/dotnet/MQTTnet/blob/master/Source/MQTTnet.Extensions.WebSocket4Net/WebSocket4NetMqttChannel.cs
+
+            //wss://iot-endpoint/mqtt
+
+            // sign with Signature Version 4
+            var signer = new SignerForQueryParameterAuth
+            {
+                EndpointUri = Host,
+                HttpMethod = "GET",
+                Service = "iotdevicegateway",
+                Region = Region
+            };
+
+            // use mqtt client WithWebSocketServer
+
             throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+
         }
     }
 }
